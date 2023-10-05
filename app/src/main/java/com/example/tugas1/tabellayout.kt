bf101bdc812a73ed0e71f755822960231a72db09
TC1009 <m.nabil.ft21@mail.umy.ac.id>
2023-10-05 18:45:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas1/tabellayout.kt
@@ -0,0 +1,11 @@
+package com.example.tugas1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tabellayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tabellayout)
+    }
+}
